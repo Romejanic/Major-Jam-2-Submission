f6164fd1a17347e283ea037de7712627f3bcb3d2
Jack Davenport <jack.davenport5011@gmail.com>
2020-07-05 03:18:42 +1000

    Added credit to title screen

--- src/com/jam/rooms/TitleRoom.java
+++ src/com/jam/rooms/TitleRoom.java
@@ -1,5 +1,8 @@
 package com.jam.rooms;
 
+import java.awt.Desktop;
+import java.net.URI;
+
 import com.jam.actors.CharacterActor;
 import com.jam.main.Main;
 import com.jam.math.Color;
@@ -11,6 +14,8 @@ import com.jam.ui.UiSprite;
 
 public class TitleRoom extends Room {
 	
+	private static final String ITCH_IO_LINK = "https://romejanic.itch.io/";
+	
 	private UiSprite logo;
 	private RoomTransition toGame;
 	
@@ -19,7 +24,7 @@ public class TitleRoom extends Room {
 	@Override
 	public void populate() {
 		// characters
-		for(int i = 0; i < 30; i++) {
+		for(int i = 0; i < 20; i++) {
 			CharacterActor character = new CharacterActor();
 			character.setWander(-30f, -10f, 30f, 10f);
 			character.transform.position.x = -30f + (float)Math.random() * 60f;
@@ -41,6 +46,16 @@ public class TitleRoom extends Room {
 		// quit button
 		this.addUiElement(new UiButton("btn_quit", 40, 0, this).addListener(() -> {
 			Main.getInstance().requestClose();
+		}));
+		// credit button
+		this.addUiElement(new UiButton("credit", -310, -270, this).addListener(() -> {
+			try {
+				Desktop.getDesktop().browse(URI.create(ITCH_IO_LINK));
+			} catch(Exception e) {
+				System.err.println("Failed to open link in browser!");
+				System.err.print("Caused by ");
+				e.printStackTrace(System.err);
+			}
 		}));
 	}
 
