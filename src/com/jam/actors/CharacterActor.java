47b1f9bc8b58761ab746ac408addbfbfce42f8bb
Jack Davenport <jack.davenport5011@gmail.com>
2020-07-05 02:49:21 +1000

    Added basic walk cycle to characters

--- src/com/jam/actors/CharacterActor.java
+++ src/com/jam/actors/CharacterActor.java
@@ -10,6 +10,8 @@ public class CharacterActor extends Actor {
 	private Sprite rightLeg;
 
 	private float animTime = 0f;
+	private float legMoveTime = 0f;
+	private int legState = 0;
 	private boolean isMale = false;
 
 	public CharacterActor() {
@@ -34,13 +36,45 @@ public class CharacterActor extends Actor {
 	@Override
 	public void update(float delta) {
 		this.animTime += delta;
+		this.legMoveTime += delta;
 		// animations
 		if(this.isMale) {
 			this.head.transform.position.y = 1.85f + 0.05f * (float)Math.sin(this.animTime*4d*Math.PI);
 		} else {
 			this.head.transform.position.y = 1.75f + 0.05f * (float)Math.sin(this.animTime*4d*Math.PI);
 		}
-		
+		if(this.legMoveTime > 0.2f) {
+			this.legMoveTime = 0;
+			this.legState = (legState + 1) % 4;
+			switch(this.legState) {
+			case 1:
+				this.leftLeg.transform.rotation.rotateZ(-0.4f);
+				this.rightLeg.transform.rotation.rotateZ(0.4f);
+				this.leftLeg.transform.position.x -= 0.05f;
+				this.leftLeg.transform.position.y += 0.1f;
+				this.rightLeg.transform.position.x += 0.05f;
+				this.rightLeg.transform.position.y += 0.3f;
+				break;
+			case 3:
+				this.leftLeg.transform.rotation.rotateZ(0.4f);
+				this.rightLeg.transform.rotation.rotateZ(-0.4f);
+				this.leftLeg.transform.position.x += 0.5f;
+				this.leftLeg.transform.position.y += 0.3f;
+				this.rightLeg.transform.position.x -= 0.6f;
+				this.rightLeg.transform.position.y += 0.1f;
+				break;
+			case 0:
+			case 2:
+			default:
+				this.leftLeg.transform.position.x = 0.2f;
+				this.leftLeg.transform.position.y = -2f;
+				this.leftLeg.transform.rotation.identity();
+				this.rightLeg.transform.position.x = 0.5f;
+				this.rightLeg.transform.position.y = -2f;
+				this.rightLeg.transform.rotation.identity();
+				break;
+			}
+		}
 	}
 
 }
