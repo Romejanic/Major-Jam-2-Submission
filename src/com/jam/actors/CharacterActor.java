cf8a09a766b3cbce3c45cd1a2b60b72930628d38
Jack Davenport <jack.davenport5011@gmail.com>
2020-07-05 02:39:48 +1000

    Complete sprite structure for characters

--- src/com/jam/actors/CharacterActor.java
+++ src/com/jam/actors/CharacterActor.java
@@ -6,10 +6,12 @@ import com.jam.room.Actor;
 public class CharacterActor extends Actor {
 
 	private Sprite head;
-	
+	private Sprite leftLeg;
+	private Sprite rightLeg;
+
 	private float animTime = 0f;
 	private boolean isMale = false;
-	
+
 	public CharacterActor() {
 		this.isMale = Math.random() > 0.5d;
 		this.animTime += (float)Math.random();
@@ -19,8 +21,16 @@ public class CharacterActor extends Actor {
 		// head sprite
 		this.head = this.addSprite(new Sprite("head_" + spriteSuffix).matchAspect());
 		this.head.transform.position.y = 1.85f;
+		// left leg
+		this.leftLeg = this.addSprite(new Sprite("leg_l")).matchAspect();
+		this.leftLeg.transform.position.x = 0.2f;
+		this.leftLeg.transform.position.y = -2f;
+		// right leg
+		this.rightLeg = this.addSprite(new Sprite("leg_r")).matchAspect();
+		this.rightLeg.transform.position.x = 0.5f;
+		this.rightLeg.transform.position.y = -2f;
 	}
-	
+
 	@Override
 	public void update(float delta) {
 		this.animTime += delta;
@@ -30,6 +40,7 @@ public class CharacterActor extends Actor {
 		} else {
 			this.head.transform.position.y = 1.75f + 0.05f * (float)Math.sin(this.animTime*4d*Math.PI);
 		}
+		
 	}
-	
+
 }
